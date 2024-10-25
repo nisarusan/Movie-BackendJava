1df6079ed049458901eb3f582346e66160fb282f
Nisaru Abak <nizar.abak@live.nl>
2024-10-26 00:32:26 +0200

    Config security some fix, not all

--- src/main/java/com/movie/moviebackend/config/SpringSecurityConfig.java
+++ src/main/java/com/movie/moviebackend/config/SpringSecurityConfig.java
@@ -1,4 +1,5 @@
 package com.movie.moviebackend.config;
+
 import com.movie.moviebackend.filter.JwtRequestFilter;
 import com.movie.moviebackend.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-/*  Deze security is niet de enige manier om het te doen.
-    In de andere branch van deze github repo staat een ander voorbeeld
- */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
@@ -30,68 +28,81 @@ public class SpringSecurityConfig {
         this.jwtRequestFilter = jwtRequestFilter;
     }
 
-    // PasswordEncoderBean. Deze kun je overal in je applicatie injecteren waar nodig.
-    // Je kunt dit ook in een aparte configuratie klasse zetten.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
-    // Authenticatie met customUserDetailsService en passwordEncoder
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
-        var auth = new DaoAuthenticationProvider();
-        auth.setPasswordEncoder(passwordEncoder);
-        auth.setUserDetailsService(customUserDetailsService);
-        return new ProviderManager(auth);
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(passwordEncoder);
+        authProvider.setUserDetailsService(customUserDetailsService);
+        return new ProviderManager(authProvider);
     }
 
-
-    // Authorizatie met jwt
     @Bean
     protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
-
         http
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(basic -> basic.disable())
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(auth ->
-                                auth
-                                        // Wanneer je deze uncomments, staat je hele security open. Je hebt dan alleen nog een jwt nodig.
-                                        .requestMatchers("/**").permitAll()
-//                                        .requestMatchers(HttpMethod.POST, "/user").hasRole("ADMIN")
-//                                        // AuthenticationController
-//                                        // UserController
-//                                        .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.GET, "/users/**/authorities").hasAnyRole("ADMIN", "USER")
-//                                        .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.PUT, "/users/**").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.POST, "/users/**/authorities").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.DELETE, "/users/**/authorities/**").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.POST, "/users/**/favorite-movies").hasAnyRole("ADMIN", "USER")
-//                                        .requestMatchers(HttpMethod.GET, "/users/**/favorite-movies").hasAnyRole("ADMIN", "USER")
-//                                        .requestMatchers(HttpMethod.DELETE, "/users/**/favorite-movies").hasAnyRole("ADMIN", "USER")
-//                                        .requestMatchers(HttpMethod.POST, "/users/**/seen-movies").hasAnyRole("ADMIN", "USER")
-//                                        .requestMatchers(HttpMethod.GET, "/users/**/seen-movies").hasAnyRole("ADMIN", "USER")
-//                                        .requestMatchers(HttpMethod.DELETE, "/users/**/seen-movies").hasAnyRole("ADMIN", "USER")
-//                                        // MovieController
-//                                        .requestMatchers(HttpMethod.POST, "/movie").hasRole("ADMIN")
-//                                        .requestMatchers(HttpMethod.GET, "/movies").hasAnyRole("USER", "ADMIN")
-//                                        .requestMatchers(HttpMethod.GET, "/movie").hasAnyRole("USER", "ADMIN")
-                                        // GenreController
-////                                        .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
-////                                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
-//                                        // Je mag meerdere paths tegelijk definieren
-//                                        .requestMatchers("/authenticated", "/authenticate", "/movies", "/").hasAnyRole("ADMIN", "USER")
-                                        .requestMatchers("/authenticated").authenticated()
-                                        .requestMatchers("/authenticate").permitAll()
-                                        .anyRequest().denyAll()
+                .authorizeHttpRequests(auth -> auth
+
+                        //Wil je alles even testen zonder authorisatie
+//                        .anyRequest().permitAll()
+
+                        // Alleen admin-toegang voor het toevoegen van genres
+                        .requestMatchers(HttpMethod.POST, "/add/genre").permitAll() //Bug.. kan niet uitvoggelen waarom.. jwt token is correct..
+
+                        // User en admin toegang voor het opvragen van films per genre
+                        .requestMatchers(HttpMethod.GET, "/movies-by-genre").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+
+                        // Toegang voor iedereen tot de authenticatie endpoints
+                        .requestMatchers(HttpMethod.GET, "/authenticated").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
+
+                        // Alleen admin toegang voor het toevoegen van films
+                        .requestMatchers(HttpMethod.POST, "/movie").hasRole("ADMIN")
+
+                        // User en admin toegang voor de lijst van films en zoekfunctionaliteit
+                        .requestMatchers(HttpMethod.GET, "/movies").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/movie").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/movie/{movieId}").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+
+                        // User en admin toegang voor het beoordelen van films en gemiddelde beoordelingen
+                        .requestMatchers(HttpMethod.POST, "/{username}/rate-movie/{movieId}").permitAll() //Bug, weet niet waarom.. pff tijdelijke optie
+                        .requestMatchers(HttpMethod.GET, "/{username}/has-rated/{movieId}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/movie/{movieId}/average-rating").permitAll() //Bug in de rating..
+
+                        // Toegang voor user en admin tot beoordeelde films
+                        .requestMatchers(HttpMethod.GET, "/{username}/rated-movies").permitAll() //Bug in de rating..
+                        // Alleen admin toegang tot gebruikersbeheer
+                        .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/{username}").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/images/upload").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/images/download").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/images/download/{username}").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/{username}").hasRole("ADMIN")
+
+                        // User en admin toegang tot autoriteiten en favorieten
+                        .requestMatchers(HttpMethod.GET, "/users/{username}/authorities").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/{username}/authorities/ROLE_USER").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/{username}/favorite-movies").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users/{username}/favorite-movies").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/{username}/favorite-movies").hasAnyRole("USER", "ADMIN")
+
+                        // Toegang tot gezien-films voor user en admin
+                        .requestMatchers(HttpMethod.POST, "/users/{username}/seen-movies").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users/{username}/seen-movies").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/{username}/seen-movies").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().denyAll()
+
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
 }
--- src/main/java/com/movie/moviebackend/controllers/GenreController.java
+++ src/main/java/com/movie/moviebackend/controllers/GenreController.java
@@ -7,7 +7,9 @@ import com.movie.moviebackend.services.GenreService;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Set;
 
 @RestController
@@ -19,12 +21,12 @@ public class GenreController {
         this.service = service;
     }
 
-    @PostMapping("/add/genre")
     public ResponseEntity<GenreDto> addGenre(@RequestBody GenreDto genreDto) {
         GenreDto savedGenreDto = service.addNewGenre(genreDto);
-        return ResponseEntity.ok().body(savedGenreDto);
+        URI uri = URI.create(
+                ServletUriComponentsBuilder.fromCurrentRequestUri().path("/" + savedGenreDto.id).toUriString());
+        return ResponseEntity.created(uri).body(savedGenreDto);
     }
-
     @GetMapping("/movies-by-genre")
     public ResponseEntity<Set<MovieDto>> getMoviesByGenre(@RequestParam String genre) {
         Set<MovieDto> movies = service.getMoviesByGenre(genre);
